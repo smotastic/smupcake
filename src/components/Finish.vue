96cbfca2c63a791b6a5a63d82fc6d5117e54ceec
Allquantor <nilssmol@gmail.com>
2020-07-30 21:30:18 +0200

    its me

--- src/components/Finish.vue
+++ src/components/Finish.vue
@@ -3,28 +3,47 @@
     <div>Congratulations, you just ate {{cupcakesAte}} Cupcakes.</div>
     <div>I hope you are proud of yourself</div>
     <div></div>
-    <div
-      class="pt"
-    >This amounts to about {{calories}} Calories or {{daysWorthOfCalories}} days worth of eating.</div>
     <div class="pt">
+      Want to know more of the Cupcakes you just ate?
+      <a href="#" @click="clickHandler">Click</a>
+    </div>
+    <transition name="bounce">
+      <Picture v-if="show" />
+    </transition>
+    <div v-if="showText">Spass, es war ein sehr gutes Projekt. Ich hoffe wir sehen uns bald mal wieder!</div>
+    <!-- <div class="pt">
       <a @click="reload" href="#">So lets eat again!</a>
     </div>
-    <div>It's cupcakes after all, what could go wrong?</div>
+    <div>It's cupcakes after all, what could go wrong?</div>-->
   </div>
 </template>
 
 <script>
+import Picture from "./Picture";
 export default {
+  components: {
+    Picture
+  },
   props: {
     cupcakesAte: Number
   },
   methods: {
     clickHandler() {
+      this.show = true;
       console.log("i dont know");
+      setTimeout(() => {
+        this.showText = true;
+      }, 1000);
     },
     reload() {
       location.reload();
     }
+  },
+  data: () => {
+    return {
+      show: false,
+      showText: false
+    };
   },
   computed: {
     calories() {
@@ -52,5 +71,23 @@ a {
 }
 .pt {
   padding-top: 20px;
+}
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 1s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0) rotate(0deg);
+  }
+  50% {
+    transform: scale(4) rotate(180deg);
+  }
+  100% {
+    transform: scale(1) rotate(360deg);
+  }
 }
 </style>
---
+++ src/components/Picture.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="joke.jpg" />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
